c78bb6873a1518fad9adf0dccf91982bd076bd52
zummon <zummon.space@gmail.com>
2023-08-30 22:04:49 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+planing to make
+
+### Design from [Canva template](https://www.canva.com/templates/EAEkUCH6gco-red-orange-organic-warm-wedding-website/)
+
+### images from [leonardo.ai](https://leonardo.ai/)
---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<div class="flex items-center flex-col h-screen justify-evenly text-red-500">
+	<span class="" style="font-family: 'Forum', cursive;">06. 20. 2025</span>
+	<h1 class="text-6xl" style="font-family: 'Forum', cursive;">Frankie & Teagan</h1>
+	<p class="max-w-xs text-center" style="font-family: 'Playfair Display', serif;">We're finally tying the knot! Come celebrate with us, and be a part of our story.</p>
+</div>
+
+<div class="flex flex-wrap h-screen text-red-500">
+	<div class="p-6 flex-1">
+		<h3 class="text-5xl mb-6" style="font-family: 'Forum', cursive;">Our Story</h3>
+		<img src="https://cdn.leonardo.ai/users/8860330a-a9fd-454d-acd4-a95c77d23b8b/generations/c19400aa-0a54-42b4-a5d0-acc054da38d6/variations/Default_Sidecut_hairVi_arcane_Sidecut_hair_teenager_rock_jacke_3_c19400aa-0a54-42b4-a5d0-acc054da38d6_1.jpg" alt="" class="">
+	</div>
+	<div class="p-6 flex-1">
+	<div class="text-xl" style="font-family: 'Forum', cursive;">
+		<p class="mb-4">
+			It's quite a funny story.
+		</p>
+		<p class="mb-4">
+			We were from entirely different worlds. We never would have met - if not for our friend-of-a-friend-of-a-friend's random dinner party. A 1 in 87 chance of meeting. Yet it happened.
+		</p>
+		<p class="">
+			And the rest is history.
+		</p>
+	</div>
+	</div>
+	<div class="p-6 flex-1">
+		<img src="https://cdn.leonardo.ai/users/ad12a655-ac8b-45b2-8d0f-6b658133bd24/generations/c4328a26-bd38-4a74-8502-d909d7a80dbb/variations/Default_Face_Wolf_icon_vector_3d_black_yellow_eyes_with_an_ora_0_c4328a26-bd38-4a74-8502-d909d7a80dbb_1.jpg" alt="" class="">
+	</div>
+</div>
